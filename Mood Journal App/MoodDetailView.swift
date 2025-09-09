52eaa10af60f1de0ccda6b18e87acc0bfe71561d
Katellyn Hyker <llyn00@yahoo.com>
2025-09-09 09:29:16 -0400

    Minor font and spacing changes

--- Mood Journal App/MoodDetailView.swift
+++ Mood Journal App/MoodDetailView.swift
@@ -12,17 +12,19 @@ struct MoodDetailView: View{
     @Binding var entry: MoodEntry
     
     var body: some View {
-            VStack(spacing: 30) {
+            VStack(spacing: 50) {
                 Text(entry.mood)
                     .font(.system(size: 60))
                 Text(entry.date.formatted(date: .abbreviated, time: .omitted))
-                    .font(.system(size: 24))
+                    .font(.system(size: 30))
                 // Add option to add notes
                 TextField("Note", text: $entry.note)
                     .padding(.horizontal)
+                    .font(.system(size: 24))
                 Spacer()
             }
             .navigationTitle("Mood Details")
+            .padding([.top], 20)
     }
 }
 
